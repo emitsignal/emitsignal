d0379e35a5b17632b3be82ccf4190c79642dcd0b
Keven Leone <kevenleone.me@gmail.com>
2026-05-27 21:25:16 -0300

    feat: update Dockerfile and package.json scripts for Prisma commands

--- packages/emitsignal-server/prisma/migrations/20260516195249_add_scheduled_delivery/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Message" ADD COLUMN "deliveredAt" DATETIME;
-ALTER TABLE "Message" ADD COLUMN "scheduledAt" DATETIME;
-
--- CreateIndex
-CREATE INDEX "Message_scheduledAt_idx" ON "Message"("scheduledAt");
