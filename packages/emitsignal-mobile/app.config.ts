0049fa3c74d1ef36769c2d39d3e21aad88a0e9c6
Keven Leone <kevenleone.me@gmail.com>
2026-06-06 22:08:38 -0300

    chore(mobile): replace app.json with app.config.ts for Expo configuration

---
+++ packages/emitsignal-mobile/app.config.ts
@@ -0,0 +1,61 @@
+import { ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+    android: {
+        adaptiveIcon: {
+            backgroundColor: '#14091e',
+            backgroundImage: './assets/images/android-icon-background.png',
+            foregroundImage: './assets/images/android-icon-foreground.png',
+            monochromeImage: './assets/images/android-icon-monochrome.png',
+        },
+        edgeToEdgeEnabled: true,
+        googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+        package: 'com.emitsignal',
+        predictiveBackGestureEnabled: false,
+    },
+    backgroundColor: '#0f0a1a',
+    experiments: {
+        reactCompiler: true,
+        typedRoutes: false,
+    },
+    extra: {
+        eas: {
+            projectId: '1424f0ee-b60d-4b18-b09c-4a3bd2740ae8',
+        },
+        router: {},
+    },
+    icon: './assets/images/icon.png',
+    ios: {
+        bundleIdentifier: 'com.emitsignal',
+        supportsTablet: true,
+    },
+    name: 'EmitSignal',
+    newArchEnabled: true,
+    orientation: 'portrait',
+    owner: 'kevenleone',
+    plugins: [
+        'expo-router',
+        [
+            'expo-splash-screen',
+            {
+                backgroundColor: '#ffffff',
+                dark: {
+                    backgroundColor: '#000000',
+                },
+                image: './assets/images/splash-icon.png',
+                imageWidth: 200,
+                resizeMode: 'contain',
+            },
+        ],
+    ],
+    scheme: 'mobile',
+    slug: 'emit-signal',
+    userInterfaceStyle: 'automatic',
+    version: '1.0.0',
+    web: {
+        favicon: './assets/images/favicon.png',
+        output: 'static',
+    },
+};
+
+export default config;
