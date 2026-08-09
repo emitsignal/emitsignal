329614586f26052f314b3fe65ec98b5e072570a3
Keven Leone <kevenleone.me@gmail.com>
2026-08-09 15:21:23 -0300

    fix(website): restore the source links to the emitsignal repo

--- packages/emitsignal-website/src/lib/links.ts
+++ packages/emitsignal-website/src/lib/links.ts
@@ -1,9 +1,7 @@
 export const REPO_URL = 'https://github.com/kevenleone/emitsignal';
 
-export const LICENSE_URL = `${REPO_URL}/tree/main?tab=AGPL-3.0-1-ov-file`;
-
+export const APP_STORE_URL = 'https://apps.apple.com/us/app/emitsignal/id6782717233';
 export const AUTHOR_URL = 'https://github.com/kevenleone';
-
 export const DOCS_URL = 'https://docs.emitsignal.com';
-
+export const LICENSE_URL = `${REPO_URL}/tree/main?tab=AGPL-3.0-1-ov-file`;
 export const STATUS_URL = 'https://status.emitsignal.com';
