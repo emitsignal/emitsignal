bbe14b4be195c6519bcbf607ad196699f23719c9
Keven Leone <kevenleone.me@gmail.com>
2026-08-21 19:29:51 -0300

    feat: add PUBLISH_BASE_URL support in Docker and mobile/web configurations

--- packages/emitsignal-mobile/lib/api.ts
+++ packages/emitsignal-mobile/lib/api.ts
@@ -7,4 +7,7 @@ export * from '@emitsignal/shared/api';
 export const API_URL =
     process.env.EXPO_PUBLIC_API_URL?.replace(/\/$/, '') ?? 'http://127.0.0.1:5001';
 
+export const PUBLISH_BASE_URL =
+    process.env.EXPO_PUBLIC_PUBLISH_BASE_URL?.replace(/\/$/, '') ?? API_URL;
+
 export const { api, getAuthToken, setAuthToken, sseMultiUrl, sseUrl } = createApiClient(API_URL);
