9cbece2eb916e3c88a438d863970eb78991a1774
Keven Leone <kevenleone.me@gmail.com>
2026-08-21 19:44:10 -0300

    feat: add shared curl and CLI publish example builders

--- packages/emitsignal-shared/src/index.ts
+++ packages/emitsignal-shared/src/index.ts
@@ -3,6 +3,7 @@ export * from './billing.ts';
 export * from './format.ts';
 export * from './message-filters.ts';
 export * from './priority.ts';
+export * from './publish-example.ts';
 export * from './topic.ts';
 export * from './url.ts';
 export * from './webhook-template.ts';
