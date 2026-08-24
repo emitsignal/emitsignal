97dad33bd737b881a8cd75b221efdcaad9e5f949
Keven Leone <kevenleone.me@gmail.com>
2026-08-23 22:11:50 -0300

    refactor(stats): simplify message total retrieval by removing await from readMessageTotal

--- packages/emitsignal-server/src/http/stats/get.ts
+++ packages/emitsignal-server/src/http/stats/get.ts
@@ -3,5 +3,5 @@ import Elysia from 'elysia';
 import { readMessageTotal } from '#/services/stats/message-counter';
 
 export const stats = new Elysia().get('/stats', async () => ({
-    messages: await readMessageTotal(),
+    messages: readMessageTotal(),
 }));
